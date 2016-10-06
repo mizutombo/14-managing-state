3f666bfe1bf1dc8a8b7d01afa4bca5defba88cf8
mizutombo <thomas.shultz52@gmail.com>
2016-10-06 13:51:41 -0700

    filled in comments on articleController file

--- starter-code/scripts/controllers/articleController.js
+++ starter-code/scripts/controllers/articleController.js
@@ -7,10 +7,11 @@
     articleView.index(ctx.articles);
   };
 
-  // COMMENT: What does this method do?  What is it's execution path? It's in the middleware chain for /articles/:id. It gets
+  // COMMENT: What does this method do?  What is it's execution path?
+  // It's in the middleware chain for /articles/:id. It gets
   //called before articlesController.index. Finds an article by
-  //the params id and sets it to the context object as ctx.articles
-  // This method loads by the id
+  //the params id and sets it to the context object as ctx.articles.
+  // This method loads by the id and is called by the findWhere method.
   articlesController.loadById = function(ctx, next) {
     var articleData = function(article) {
       ctx.articles = article;
@@ -21,6 +22,10 @@
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
+  // This method is in the middleware chain for /author/:authorName.
+  // Finds an article by the params 'author' and sets it to the context
+  // object as ctx.articles.
+  // This loads by the author and is called by the findWhere method.
   articlesController.loadByAuthor = function(ctx, next) {
     var authorData = function(articlesByAuthor) {
       ctx.articles = articlesByAuthor;
@@ -33,6 +38,9 @@
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
+  // This method is in the middleware chain for /category/:categoryName.
+  // Finds an article by the params category and sets it to ctx.articles.
+  // This loads by the category and is called by the findWhere method.
   articlesController.loadByCategory = function(ctx, next) {
     var categoryData = function(articlesInCategory) {
       ctx.articles = articlesInCategory;
@@ -43,6 +51,10 @@
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
+  // This method is in the middleware chain for the index page.
+  // Finds all articles and sets them to the ctx.articles if we
+  // have an array length for Article.all. Otherwise, it uses
+  // articleData as the callback function for Article.fetchAll.
   articlesController.loadAll = function(ctx, next) {
     var articleData = function(allArticles) {
       ctx.articles = Article.all;
